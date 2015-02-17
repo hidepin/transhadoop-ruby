be48b1e3daa952d56a6fee6181d2c361855fe53f
Hidetoshi IMAI <imai@func.org>
2015-02-17 22:33:37 +0900

    add hadoop put

--- transhadoop.rb
+++ transhadoop.rb
@@ -3,13 +3,24 @@
 # coding: uitf-8
 
 require 'bundler'
+require 'net/http'
 
 Bundler.require
 
-remotehost="localhost"
+remotehost="127.0.0.1"
 remoteuser="dev"
 remotepasswd="dev123"
+httpfshost="127.0.0.1"
+httpfsport=14000
+httpfsuser="hadoop"
 
+remotefile = ARGV[0]
+hadoopfile = ARGV[1]
 
-Net::SCP.download!(remotehost, remoteuser,  ARGV[0], ".",
+Net::SCP.download!(remotehost, remoteuser,  remotefile, ".",
                    :ssh => { :password => remotepasswd })
+
+file = File.open(File.basename(remotefile))
+http = Net::HTTP.start(httpfshost, httpfsport)
+response = http.put("/webhdfs/v1#{hadoopfile}?user.name=#{httpfsuser}&op=create&data=true", file.read, {'content-type' => 'application/octet-stream'})
+puts response
